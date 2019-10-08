18c608a11841f5b58ff7aa1870ec7d6911dbbdd1
SergeiBychkov <54210714+SergeiBychkov@users.noreply.github.com>
2019-10-08 22:20:20 +0300

    123
    321

--- js/script.js
+++ js/script.js
@@ -2,38 +2,49 @@ var search = document.querySelector(".button-active");
 
 var popup = document.querySelector(".pop-up");
 
-var datein = document.querySelector("[name=arrival-date]")
+var datein = document.querySelector("[name=arrival-date]");
 
-var dateout = document.querySelector("[name=departure-date]")
+var dateout = document.querySelector("[name=departure-date]");
 
-var form = popup.querySelector("form")
+var form = popup.querySelector("form");
 
+function closeModal() {
+  popup.classList.add("modal-hide");
+};
+
+closeModal();
 
 search.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-close");
-  datein.focus ();
+  if (!popup.classList.contains("modal-hide")) {
+    popup.classList.add("modal-close");
+    setTimeout(function () {
+      popup.classList.add("modal-hide");
+      popup.classList.remove("modal-close");
+    }, 600);
+  }
+  else {
+    popup.classList.remove("modal-hide");
+  }
 });
 
 form.addEventListener("submit", function (evt) {
   if (!datein.value || !dateout.value) {
     evt.preventDefault();
     popup.classList.add("modal-error");
+    setTimeout(function () {
+      popup.classList.remove("modal-error")
+    }, 550);
     console.log("нужно выбрать даты");
   }
 });
 
-
-
-
-
-/* клавиши ескейпа
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("pop-up")) {
-      popup.classList.remove("pop-up");
+    if (!popup.classList.contains("modal-close")) {
+      popup.classList.add("modal-close");
     }
   }
 });
-*/
+
